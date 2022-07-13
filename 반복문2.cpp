d7ec9a48de1f77969ebca2f0ac8287fda669a743
shinsam <39876724+shinsam@users.noreply.github.com>
2022-07-13 10:10:30 +0900

    Add files via upload
    수정 테스트

--- 반복문2.cpp
+++ 반복문2.cpp
@@ -2,17 +2,18 @@
 main(){
 	// 5 , 3  : 12가 나오면 정답 
 	int i , n , sum = 0;
-    int large = i;
+	int large = i;
     
 	printf("큰  수 입력 : ");	
-    scanf("%d", &i);
+	scanf("%d", &i);
 	
 	printf("작은수 입력 : ");	
-    scanf("%d", &n);
+	scanf("%d", &n);
     
-    while(i >= n){
+	while(i >= n){
     	sum = sum + i;  //sum += i
     	i--;
 	}
 	printf("%d부터 %d까지의 합은 %d입니다\n",large, n, sum); 
+	//add 
 }
